2316be80dd03f0bf5833c574cefbec324811da44
Himesh Baraik <134226469+HimeshBaraik@users.noreply.github.com>
2024-08-29 03:14:33 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -14,12 +14,12 @@ def fetch_medal_tally(df, year, country):
         temp_df = medal_df[medal_df['Year'] == int(year)]
         
     if year != 'Overall' and country != 'Overall':
-        temp_df = medal_df[(medal_df['Year'] == year) & (medal_df['region'] == country)]
+        temp_df = medal_df[(medal_df['Year'] == int(year)) & (medal_df['region'] == country)]
 
     if flag == 1:
-        x = temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Year').reset_index()
+        x = temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].fillna(0).sort_values('Year').reset_index()
     else:
-        x = temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending=False).reset_index()
+        x = temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].fillna(0).sort_values('Gold', ascending=False).reset_index()
 
     x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
 
